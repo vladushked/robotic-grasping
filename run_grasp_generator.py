550dbd0203be2de3d95c5a575408767ab4511a7e
Vladislav Plotnikov <vladik1209@gmail.com>
2022-02-14 20:11:01 +0300

    f

--- run_grasp_generator.py
+++ run_grasp_generator.py
@@ -3,7 +3,7 @@ from inference.grasp_generator import GraspGenerator
 if __name__ == '__main__':
     generator = GraspGenerator(
         cam_id=830112070066,
-        saved_model_path='trained-models/jacquard-rgbd-grconvnet3-drop0-ch32/epoch_48_iou_0.93',
+        saved_model_path='trained-models/cornell-randsplit-rgbd-grconvnet3-drop1-ch16/epoch_30_iou_0.97',
         visualize=True
     )
     generator.load_model()
